c9d0a50786bb1f18b2e89c2b7a5f701203e5bb8d
natrodrigo <nata.rodrigo.p@gmail.com>
2021-02-10 01:13:15 -0300

    teste para adição de imagens.

--- index.js
+++ index.js
@@ -159,17 +159,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
         document.querySelector('#realcont').innerHTML = (
-        "R<sub>1</sub> = " + rp + "Ω"+"<br>"+
+        "<img src='https://imgur.com/1H6TIY9.png'alt='Circuito Real'><br>R<sub>1</sub> = " + rp + "Ω"+"<br>"+
         "X<sub>l1</sub> = " + xp + "Ω"+"<br>"+
         "R<sub>c</sub> = " + rc_p + "Ω"+"<br>"+
         "X<sub>m</sub> = " + xm_p + "Ω"+"<br>"+
         "R<sub>2</sub> = " + rs + "Ω"+"<br>"+
-        "R<sub>l2</sub> = " + xs + "Ω"+"<br>"+
+        "X<sub>l2</sub> = " + xs + "Ω"+"<br>"+
         "Perdas no núcleo = " + pnucleo + "W"+"<br>"+
         "Perdas no cobre = " + pcu + "W");
         
         document.querySelector('#tp').innerHTML = (
-        "V<sub>p</sub> = " + vp + "V"+"<br>"+
+        "<img src='https://imgur.com/Z5ZJB4Q.png' alt='Circuito T Primário'><br>V<sub>p</sub> = " + vp + "V"+"<br>"+
         "aV<sub>s</sub> = " + vp + "V"+"<br>"+
         "R<sub>p</sub> = " + rp + "Ω"+"<br>"+
         "jX<sub>p</sub> = " + xp + "Ω"+"<br>"+
@@ -179,7 +179,7 @@ document.addEventListener('DOMContentLoaded', function(){
         "ja²X<sub>s</sub> = " + xp + "Ω");
 
         document.querySelector('#ts').innerHTML = (
-        "V<sub>p</sub>/a = " + vs + "V"+"<br>"+
+        "<img src='https://i.imgur.com/K7Vfuup.png' alt='Circuito T Secundário'><br>V<sub>p</sub>/a = " + vs + "V"+"<br>"+
         "R<sub>p</sub>/a² = " + rs + "V"+"<br>"+
         "jX<sub>p</sub>/a² = " + xs + "Ω"+"<br>"+
         "R<sub>c</sub>/a² = " + rc_s + "Ω"+"<br>"+
@@ -189,20 +189,20 @@ document.addEventListener('DOMContentLoaded', function(){
         "jX<sub>m</sub>/a² = " + xm_s + "Ω");
 
         document.querySelector('#lp').innerHTML = (
-        "V<sub>p</sub> = " + vp + "V"+"<br>"+
+        "<img src='https://i.imgur.com/KZeh4Ty.png' alt='Circuito L Primário'><br>V<sub>p</sub> = " + vp + "V"+"<br>"+
         "R<sub>c</sub> = " + rc_p + "Ω"+"<br>"+
         "jX<sub>m</sub> = " + xm_p + "Ω"+"<br>"+
         "aV<sub>s</sub> = " + vp + "V"+"<br>"+
         "R<sub>eq(p)</sub> = " + req_p + "Ω"+"<br>"+
-        "X<sub>eq(p)</sub> = " + xeq_p + "Ω");
+        "jX<sub>eq(p)</sub> = " + xeq_p + "Ω");
         
         document.querySelector('#ls').innerHTML = (
-        "V<sub>p</sub>/a = " + vs + "V"+"<br>"+
+        "<img src='https://i.imgur.com/rknbjLL.png' alt='Circuito L Secundário'><br>V<sub>p</sub>/a = " + vs + "V"+"<br>"+
         "R<sub>c</sub>/a² = " + rc_s + "Ω"+"<br>"+
         "jX<sub>m</sub>/a²= " + xm_s + "Ω"+"<br>"+
         "V<sub>s</sub> = " + vs + "V"+"<br>"+
         "R<sub>eq(s)</sub> = " + req_s + "Ω"+"<br>"+
-        "X<sub>eq(s)</sub> = " + xeq_s + "Ω");
+        "jX<sub>eq(s)</sub> = " + xeq_s + "Ω");
         
 
         document.getElementById("divforms").style.display = "none";
